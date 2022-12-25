c563f80012cabb0d46a1a4822d0a979be580431f
anandappcleus <anand.klap@gmail.com>
2022-12-25 23:03:18 +0530

    commit Version 2 and version 3 of my part3 tutorial
    Classes|Objects|Enumerations|Structure|Protocol|Extension

--- SwiftBasicConcept-Part03.playground/Contents.swift
+++ SwiftBasicConcept-Part03.playground/Contents.swift
@@ -196,3 +196,310 @@ print(mappedNumbers)
 
 let sortedNumbers = numbers.sorted { $0 > $1 }
 print(sortedNumbers)
+
+
+
+//MARK: -  Part 3 - 2nd verion
+
+/*=================================================CodeWithAnand=========================================================*/
+
+print("Do Like and Subscribe my channel CodeWithAnand or @bikkypanday8999")
+
+let MyYoutubeChannel_ = URL(string: "https://www.youtube.com/@bikkypanday8999")
+
+
+
+
+//MARK: - Objects and Classes
+
+class Shape {
+    var numberOfSides = 0
+    func simpleDescription() -> String {
+        return "A shape with \(numberOfSides) sides."
+    }
+}
+
+var shape = Shape()
+shape.numberOfSides = 7
+var shapeDescription = shape.simpleDescription()
+print(shapeDescription)
+
+
+
+class NamedShape {
+    var numberOfSides: Int = 0
+    var name: String
+
+    init(name: String) {
+        self.name = name
+    }
+
+    func simpleDescription() -> String {
+        return "A shape with \(numberOfSides) sides."
+    }
+}
+/*Notice how self is used to distinguish the name property from the name argument to the initializer. The arguments to the initializer are passed like a function call when you create an instance of the class. Every property needs a value assigned—either in its declaration (as with numberOfSides) or in the initializer (as with name).*/
+
+//MARK: - Single Level inhertance
+
+class Square: NamedShape {
+    var sideLength: Double
+
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 4
+    }
+
+    func area() -> Double {
+        return sideLength * sideLength
+    }
+
+    override func simpleDescription() -> String {
+        return "A square with sides of length \(sideLength)."
+    }
+}
+
+let test = Square(sideLength: 5.2, name: "my test square")
+test.area()
+test.simpleDescription()
+
+//MARK: - In addition to simple properties that are stored, properties can have a getter and a setter.
+
+class EquilateralTriangle: NamedShape {
+    var sideLength: Double = 0.0
+
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 3
+    }
+
+    var perimeter: Double {
+        get {
+            return 3.0 * sideLength
+        }
+        set {
+            sideLength = newValue / 3.0
+        }
+    }
+
+    override func simpleDescription() -> String {
+        return "An equilateral triangle with sides of length \(sideLength)."
+    }
+}
+var triangle = EquilateralTriangle(sideLength: 3.1, name: "a triangle")
+print(triangle.perimeter)
+// Prints "9.3"
+triangle.perimeter = 9.9
+print(triangle.sideLength)
+// Prints "3.3000000000000003"
+
+/*In the setter for perimeter, the new value has the implicit name newValue. You can provide an explicit name in parentheses after set.
+ 
+ Notice that the initializer for the EquilateralTriangle class has three different steps:
+
+ Setting the value of properties that the subclass declares.
+
+ Calling the superclass’s initializer.
+
+ Changing the value of properties defined by the superclass. Any additional setup work that uses methods, getters, or setters can also be done at this point.
+*/
+
+
+//MARK: - Enumerations and Structures
+//Use enum to create an enumeration. Like classes and all other named types, enumerations can have methods associated with them.
+
+enum Rank: Int {
+    case ace = 1
+    case two, three, four, five, six, seven, eight, nine, ten
+    case jack, queen, king
+
+    func simpleDescription() -> String {
+        switch self {
+        case .ace:
+            return "ace"
+        case .jack:
+            return "jack"
+        case .queen:
+            return "queen"
+        case .king:
+            return "king"
+        default:
+            return String(self.rawValue)
+        }
+    }
+}
+let ace = Rank.ace
+print(ace)
+let aceRawValue = ace.rawValue
+print(aceRawValue)
+
+/*By default, Swift assigns the raw values starting at zero and incrementing by one each time, but you can change this behavior by explicitly specifying values. In the example above, Ace is explicitly given a raw value of 1, and the rest of the raw values are assigned in order. You can also use strings or floating-point numbers as the raw type of an enumeration. Use the rawValue property to access the raw value of an enumeration case.*/
+
+/*Use the init?(rawValue:) initializer to make an instance of an enumeration from a raw value. It returns either the enumeration case matching the raw value or nil if there’s no matching Rank.
+ */
+let convertedRank = Rank(rawValue: 2)
+print(convertedRank!.simpleDescription())
+//print(convertedRank!.simpleDescription())
+if let convertedRank = Rank(rawValue: 3) {
+    let threeDescription = convertedRank.simpleDescription()
+    print(threeDescription)
+}
+
+enum Suit {
+    case spades, hearts, diamonds, clubs
+
+    func simpleDescription() -> String {
+        switch self {
+        case .spades:
+            return "spades"
+        case .hearts:
+            return "hearts"
+        case .diamonds:
+            return "diamonds"
+        case .clubs:
+            return "clubs"
+        }
+    }
+}
+let hearts = Suit.hearts
+let heartsDescription = hearts.simpleDescription()
+print(heartsDescription)
+/*Notice the two ways that the hearts case of the enumeration is referred to above: When assigning a value to the hearts constant, the enumeration case Suit.hearts is referred to by its full name because the constant doesn’t have an explicit type specified. Inside the switch, the enumeration case is referred to by the abbreviated form .hearts because the value of self is already known to be a suit. You can use the abbreviated form anytime the value’s type is already known.*/
+
+
+//MARK: -  Part 3 - 3nd verion
+
+/*=================================================CodeWithAnand=========================================================*/
+
+print("Do Like and Subscribe my channel CodeWithAnand or @bikkypanday8999")
+
+let MyYoutubeChannel__ = URL(string: "https://www.youtube.com/@bikkypanday8999")
+
+//MARK: - Enum with Associated type
+
+/*associated values as behaving like stored properties of the enumeration case instance. For example, consider the case of requesting the sunrise and sunset times from a server. The server either responds with the requested information, or it responds with a description of what went wrong.**/
+
+enum ServerResponse {
+    case result(String, String)
+    case failure(String)
+}
+
+let success = ServerResponse.result("6:00 am", "8:09 pm")
+let failure = ServerResponse.failure("Out of cheese.")
+
+switch success {
+case let .result(sunrise, sunset):
+    print("Sunrise is at \(sunrise) and sunset is at \(sunset).")
+case let .failure(message):
+    print("Failure...  \(message)")
+}
+// Prints "Sunrise is at 6:00 am and sunset is at 8:09 pm."
+
+
+//MARK: - Structure
+
+/*Structures support many of the same behaviors as classes, including methods and initializers. One of the most important differences between structures and classes is that structures are always copied when they’re passed around in your code, but classes are passed by reference.*/
+
+struct Card {
+    var rank: Rank
+    var suit: Suit
+    func simpleDescription() -> String {
+        return "The \(rank.simpleDescription()) of \(suit.simpleDescription())"
+    }
+}
+let threeOfSpades = Card(rank: .three, suit: .spades)
+let threeOfSpadesDescription = threeOfSpades.simpleDescription()
+print(threeOfSpadesDescription)
+
+//MARK: - Protocols and Extensions
+
+//Use protocol to declare a protocol.
+
+protocol ExampleProtocol {
+    var simpleDescription: String { get }
+    mutating func adjust()
+}
+
+//Classes, enumerations, and structures can all adopt protocols.
+
+class SimpleClass: ExampleProtocol {
+    var simpleDescription: String = "A very simple class."
+    var anotherProperty: Int = 69105
+    func adjust() {
+        simpleDescription += "  Now 100% adjusted."
+    }
+}
+var a = SimpleClass()
+a.adjust()
+let aDescription = a.simpleDescription
+print(aDescription)
+
+
+struct SimpleStructure: ExampleProtocol {
+    var simpleDescription: String = "A simple structure"
+    mutating func adjust() {
+        simpleDescription += " (adjusted)"
+    }
+}
+var b = SimpleStructure()
+b.adjust()
+let bDescription = b.simpleDescription
+print(bDescription)
+
+//MARK: CLASS VS STRUCTURE
+
+struct Coordinates {
+    var lat: Double
+    var lng: Double
+}
+
+var point1:Coordinates = Coordinates(lat: 5.519, lng: 5.7599)
+// here, we are setting the values of point2 to be the value of point1
+var point2:Coordinates = point1
+
+point2.lat = 6.45
+point2.lng = 8.211
+
+print(point2) // expected output: Coordinates(lat: 6.45, lng: 8.211)
+print(point1) // expected output: Coordinates(lat: 5.519, lng: 5.7599)
+
+
+class User {
+  var  firstName: String
+  var  lastName: String
+  var  gender: String
+  var  age: Int
+
+    init(firstName: String, lastName: String, gender: String, age: Int) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.gender = gender
+        self.age = age
+    }
+}
+
+var user1:User  = User(firstName:  "Ejiro", lastName: "Asiuwhu", gender: "male", age: 29)
+// user2 now holds the same value as user1
+var user2:User  = user1
+
+user1.age = 30
+
+print(user1.age) // expected output: 30
+print(user2.age) // expected output: 30
+
+
+/*
+ When to use classes vs. structs in Swift
+ The apple official documentation largely recommends that users should use structs by default. This is mostly because structs are much safer and bug-free, especially in a multithreaded environment. Structs are also preferable if they are relatively small and copyable because copying structs is safer than having multiple references to the same instance.
+
+ When choosing between structs and classes, it’s important to remember the key differences:
+ 
+ Classes are reference types, and structs are value types
+ If class inheritance is not needed, structs are faster and more memory efficient
+ Use structs for unique copies of an object with independent states
+ Use structs when working with a few, relatively simple data values
+ Use classes to control an object’s identity
+ Use structs when there is no need to control an object’s identity
+*/
